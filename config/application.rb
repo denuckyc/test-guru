8c2f1e025710938eb12462cb5eac6919f7509afc
den <gogivg@yandex.ru>
2020-04-28 18:58:39 +0300

    completing task 1

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TestGuru
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ru
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
