9e436a97981c138933e12f422c5a2e65d96a7607
den <gogivg@yandex.ru>
2020-05-09 03:43:44 +0300

    fix tab size

--- db/migrate/20200509002948_add_index_level_on_title_and_level.rb
+++ db/migrate/20200509002948_add_index_level_on_title_and_level.rb
@@ -1,5 +1,5 @@
 class AddIndexLevelOnTitleAndLevel < ActiveRecord::Migration[6.0]
   def change
-  	add_index(:tests, [:title, :level], unique: true)
+    add_index(:tests, [:title, :level], unique: true)
   end
 end
