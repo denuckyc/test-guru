f536dbfefe522bf5580f6f220704e85c0fa15a54
den <gogivg@yandex.ru>
2020-05-13 15:33:56 +0300

    fix after cr - add index and rename scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,13 +8,13 @@ class Test < ApplicationRecord
   scope :easy,   -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard,   -> { where(level: 5..Float::INFINITY) }
-  scope :sort_by_title, ->(title) { joins(:category).where(categories: { title: title }) }
+  scope :filter_by_title, ->(title) { joins(:category).where(categories: { title: title }) }
 
   validates :title, presence: true
   validates_numericality_of :level, only_integer: true, greater_than_or_equal_to: 0
   validates :title, uniqueness: { scope: :level }
 
   def self.sort_tests(category)
-    sort_by_title(category).order(title: :DESC).pluck(:title)
+    filter_by_title(category).order(title: :DESC).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   has_many :passing_test
   has_many :tests, through: :passing_test
 
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   def passed_tests(level)
     PassingTest.joins(:test).where(tests: {level: level}).order(title: :DESC).pluck('tests.title')
